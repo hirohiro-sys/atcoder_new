56809e02da4a2ebc68558266cb7fc67250eb6b41
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-06-18 20:33:30 +0900

    Create ABC303b.py

---
+++ B_practice/ABC303b.py
@@ -0,0 +1,13 @@
+#入力
+n,m = map(int,input().split())
+li = [list(map(int,input().split())) for _ in range(m)]
+#隣り合うペアを格納する集合（順番は考慮しない）
+t = set()
+
+for i in range(m):
+  for j in range(n-1):
+    a,b = li[i][j],li[i][j+1]
+    #順番は考慮しないため左に小さい値,右に大きい値をおくと決める
+    t.add((min(a,b),max(a,b)))
+#全体の組み合わせの数から隣り合う組み合わせの数を引く
+print(n*(n-1)//2-len(t))
