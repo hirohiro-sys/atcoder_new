0301956fd1fd2bc1866d8b222aab355f69b927f4
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-06-18 20:04:43 +0900

    B問題の練習

--- B_practice/ABC302b.py
+++ B_practice/ABC302b.py
@@ -4,7 +4,7 @@ li = [input() for _ in range(h)]
 
 for i in range(h):
     for j in range(w):
-        #8方向
+        #8方向に移動する際の変化量を示す
         for x,y in [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]:
             #文字が一致 & 盤面の範囲内かをチェック
             if all(0<=i+n*x<h and 0<=j+n*y<w and li[i+n*x][j+n*y]==s for n,s in enumerate("snuke")):
